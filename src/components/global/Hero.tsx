48e63a606da566cec4e9d6ccf68bf9a5f7c00c0e
victor <vic_o@icloud.com>
2025-12-03 07:19:25 +0100

    portfolio has been updated 03-12-2025

--- src/components/global/Hero.tsx
+++ src/components/global/Hero.tsx
@@ -29,25 +29,6 @@ export default function Hero() {
             {/* <div className="relative max-w-[400px] h-[1px] bg-gradient-to-r from-transparent via-blue-500 via-red-500 via-purple-500 to-transparent mb-8 overflow-hidden">
               <div className="absolute top-0 left-0 w-full h-full animate-[moveLight_2s_linear_infinite] bg-gradient-to-r from-transparent via-white to-transparent"></div>
             </div> */}
-
-            {/* <p className="">
-              <span className="mr-3">
-                Disponible pour des missions en
-                <span className="font-semibold"> freelance</span>.
-              </span>
-
-               <span className="">
-                <Button
-                  variant="link"
-                  style={{ textDecoration: "none" }}
-                  className="group text-blue-600 font-bold text-lg p-0 transition-all duration-300 hover:text-blue-700 hover:scale-105 hover:rotate-2 "
-                >
-                  mon CV
-                  <Download className="opacity-0 group-hover:opacity-100 transition-all duration-300" />
-                </Button>
-              </span> 
-            </p>
-            */}
           </div>
         </div>
       </div>
